f4326acda27b100aff7e4eeefac0026a9480c580
kumar-kunal <30340538+kumar-kunal@users.noreply.github.com>
2024-07-23 13:04:58 +0530

    Delete bot directory

--- bot/bot.constant.js
+++
@@ -1,7 +0,0 @@
-const { replyToMessage } = require("./handlers/message-replier.handler");
-
-const ChannelToActionMapping = {
-    CH_1 : replyToMessage
-}
-
-module.exports = {ChannelToActionMapping}
